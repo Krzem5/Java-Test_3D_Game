c264ecd0e41258e80cb18c023aab8dd0ef71d588
Krzem <krzem5.dev@gmail.com>
2021-01-12 20:30:04 +0100

    Push Update 01/12/2021, 20:03:43

--- index.bat
+++ index.bat
@@ -1,5 +1,21 @@
-echo off
-echo NUL>_.class&&del /s /f /q *.class
+@echo off
 cls
-javac -cp com/krzem/test_3d_game/modules/jogl-all.jar;com/krzem/test_3d_game/modules/jogl-all-natives-windows-i586.jar;com/krzem/test_3d_game/modules/gluegen-rt.jar;com/krzem/test_3d_game/modules/gluegen-rt-natives-windows-i586.jar; com/krzem/test_3d_game/Main.java&&java -cp com/krzem/test_3d_game/modules/jogl-all.jar;com/krzem/test_3d_game/modules/jogl-all-natives-windows-i586.jar;com/krzem/test_3d_game/modules/gluegen-rt.jar;com/krzem/test_3d_game/modules/gluegen-rt-natives-windows-i586.jar; com/krzem/test_3d_game/Main
-start /min cmd /c "echo NUL>_.class&&del /s /f /q *.class"
+if exist build rmdir /s /q build
+mkdir build
+cd src
+javac -cp com/krzem/test_3d_game/modules/jogl-all.jar;com/krzem/test_3d_game/modules/jogl-all-natives-windows-i586.jar;com/krzem/test_3d_game/modules/gluegen-rt.jar;com/krzem/test_3d_game/modules/gluegen-rt-natives-windows-i586.jar; -d ../build com/krzem/test_3d_game/Main.java&&jar cvmf ../manifest.mf ../build/test_3d_game.jar -C ../build *&&goto run
+cd ..
+goto end
+:run
+cd ..
+pushd "build"
+for /D %%D in ("*") do (
+	rd /S /Q "%%~D"
+)
+for %%F in ("*") do (
+	if /I not "%%~nxF"=="test_3d_game.jar" del "%%~F"
+)
+popd
+cls
+java -jar build/test_3d_game.jar
+:end
